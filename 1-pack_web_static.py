85cefd4427878c4b6ea2b0b6d24321e39496171c
Viet Tran <vietnamtran@gmail.com>
2020-08-17 18:44:19 +0000

    Fix Docstring indentation

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,9 +7,9 @@ from datetime import datetime
 
 
 def do_pack():
-"""Fabric script that generates a .tgz archive from the contents
-of the web_static folder of your AirBnB Clone repo
-"""
+    """Fabric script that generates a .tgz archive from the contents
+    of the web_static folder of your AirBnB Clone repo
+    """
     dt = datetime.now()
     dt_format = dt.strftime("%Y-%m-%d %H:%M:%S")
     archive = "web_static_" + dt_format + ".tgz"
