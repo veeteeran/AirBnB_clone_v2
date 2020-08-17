65b66a1cbe8671344e5920f846fe5a510f0b4a78
Viet Tran <vietnamtran@gmail.com>
2020-08-17 17:07:59 +0000

    Task 0 and 1

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Sets up your web servers for the deployment of web_static
+
+apt-get update
+apt-get -y install nginx
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/current/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "simple content, to test your Nginx configuration" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+sed -i '/^\tserver_name.*/a \\n\tlocation /hbnb_static/ \{\n\t\talias /data/web_static/current/;\n\t\}\n' /etc/nginx/sites-available/default
+service nginx restart
---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Fabric script that generates a .tgz archive
+"""
+from fabric.api import local
+from datetime import datetime
+
+
+
+def do_pack():
+"""Fabric script that generates a .tgz archive from the contents
+of the web_static folder of your AirBnB Clone repo
+"""
+    dt = datetime.now()
+    dt_format = dt.strftime("%Y-%m-%d %H:%M:%S")
+    archive = "web_static_" + dt_format + ".tgz"
+
+    if not isdir("mkdir /versions"):
+        local("mkdir /versions")
+
+    try:
+        local('tar -cfv {} versions/{}'.format(archive, archive))
+        path = "versions/" + archive
+    except:
+        return None
