4e9a4d045db407e1908eadaac6f527e5f656dad7
Viet Tran <vietnamtran@gmail.com>
2020-08-17 22:06:15 +0000

    FIrst commit with tested code

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Distributes an archive to your web servers,
+using the function do_deploy
+"""
+from fabric.api import env, put, run
+from datetime import datetime
+
+
+env.user = 'ubuntu'
+web01, web02 = '35.190.170.193', '3.80.222.135'
+env.hosts = [web01, web02]
+
+
+def do_deploy(archive_path):
+    """ do_deploy docstring """
+    try:
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+        #
+        archive = archive_path.split('/')[1]
+        output = "/data/web_static/releases/{}".format(archive.split('.')[0])
+        run("mkdir -p {}/".format(output))
+        # Uncompress the archive
+        run("tar -xzvf /tmp/{} -C {}/".format(archive, output))
+        # Delete the archive from the web server
+        run("rm -rf /tmp/{}".format(archive))
+        # Delete the symbolic link
+        run("rm -rf /data/web_static/current")
+        # Create a new the symbolic link
+        filename = archive.split('.')[0]
+        run("ln -sf /data/web_static/releases/{}/ 
+            /data/web_static/current".format(filename))
+        print("New version deployed!")
+        return True
+    except:
+        return False
