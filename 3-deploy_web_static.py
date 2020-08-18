2e1b7844587b42cf13ee4556d9acd0bce80098e1
Viet Tran <vietnamtran@gmail.com>
2020-08-18 00:09:14 +0000

    Name the module properly

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Creates and distributes an archive to your web servers
 """
+from fabric.state import env
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
@@ -8,7 +9,7 @@ env.user = 'ubuntu'
 web01, web02 = '35.190.170.193', '3.80.222.135'
 env.hosts = [web01, web02]
 
-def do_deploy():
+def deploy():
     """ do_deploy docstring """
     archive_path = do_pack()
     from os.path import isfile
