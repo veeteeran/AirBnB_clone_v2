2910811c5737ebc5b3397c768b90ff247890e4a5
Viet Tran <vietnamtran@gmail.com>
2020-08-18 00:57:48 +0000

    Trying to move imports around

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """Creates and distributes an archive to your web servers
 """
-from fabric.state import env
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
+from fabric.api import env
 
 
 env.user = 'ubuntu'
 web01, web02 = '35.190.170.193', '3.80.222.135'
 env.hosts = [web01, web02]
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 
 def deploy():
     """ do_deploy docstring """
     archive_path = do_pack()
-    from os.path import isfile
-    if not isfile(archive_path):
+    if archive_path is None:
         return False
-    value = do_deploy(archive_path)
-    return value
+    return do_deploy(archive_path)
