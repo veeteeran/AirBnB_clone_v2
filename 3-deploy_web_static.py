e2ba7005e6f12ca159bf428c34968143f567fca0
Viet Tran <vietnamtran@gmail.com>
2020-08-18 00:12:11 +0000

    pep8 fixes

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,15 +5,17 @@ from fabric.state import env
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
+
 env.user = 'ubuntu'
 web01, web02 = '35.190.170.193', '3.80.222.135'
 env.hosts = [web01, web02]
+
 
 def deploy():
     """ do_deploy docstring """
     archive_path = do_pack()
     from os.path import isfile
-    if not isfile (archive_path):
+    if not isfile(archive_path):
         return False
     value = do_deploy(archive_path)
     return value
