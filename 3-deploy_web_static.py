a2ce7204a1de20d28c82430022c9d0d79df83931
Viet Tran <vietnamtran@gmail.com>
2020-08-18 00:04:05 +0000

    First untested commit

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Creates and distributes an archive to your web servers
+"""
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+env.user = 'ubuntu'
+web01, web02 = '35.190.170.193', '3.80.222.135'
+env.hosts = [web01, web02]
+
+def do_deploy():
+    """ do_deploy docstring """
+    archive_path = do_pack()
+    from os.path import isfile
+    if not isfile (archive_path):
+        return False
+    value = do_deploy(archive_path)
+    return value
