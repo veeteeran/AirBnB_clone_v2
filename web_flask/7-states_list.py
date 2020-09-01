6c964057ec81307fcd98fbdb61d27c3d582b969e
Viet Tran <vietnamtran@gmail.com>
2020-09-01 20:04:43 +0000

    Tested task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+""" Task 8 doc """
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(db):
+    """ Docstring for teardown """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ display a HTML page: (inside the tag BODY) """
+    states = storage.all()
+
+    return render_template('7-states_list.html', states=states)
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=5000,
+        debug=True
+    )
