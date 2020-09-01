d7f9f3489a01dafbbf7718f81e70f494718549b2
Viet Tran <vietnamtran@gmail.com>
2020-09-01 21:25:43 +0000

    Refactor. It was so simple

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,9 +15,8 @@ def teardown(db):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ display a HTML page: (inside the tag BODY) """
-    states = storage.all()
 
-    return render_template('7-states_list.html', states=states)
+    return render_template('7-states_list.html', states=storage.all('State'))
 
 if __name__ == '__main__':
     app.run(
