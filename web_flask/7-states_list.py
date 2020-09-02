f86182045caa3e44f9de6376bc305b5fb5b53be3
Viet Tran <vietnamtran@gmail.com>
2020-09-02 14:51:48 +0000

    Refactor, pull only values from storage, use sort instead of dictsort

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,8 +15,9 @@ def teardown(self):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ display a HTML page: (inside the tag BODY) """
+    states = storage.all('State').values()
 
-    return render_template('7-states_list.html', states=storage.all('State'))
+    return render_template('7-states_list.html', states=states)
 
 if __name__ == '__main__':
     app.run(
