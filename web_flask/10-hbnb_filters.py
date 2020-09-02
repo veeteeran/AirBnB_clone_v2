2a78eeaea652ae2107dfa4d1e06c758e8fd6d00a
Viet Tran <vietnamtran@gmail.com>
2020-09-02 23:41:41 +0000

    Delete id arg in task 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -2,6 +2,7 @@
 """ Task 8 doc """
 from models import storage
 from models.state import State
+from models.amenity import Amenity
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -13,7 +14,7 @@ def teardown(self):
 
 
 @app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters(id):
+def hbnb_filters():
     """ display a HTML page: like 6-index.html """
     states = storage.all('State').values()
     amenities = storage.all('Amenity').values()
