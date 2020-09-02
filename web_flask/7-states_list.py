9d0891111452773adfb0a7bb22bf4fd6830d9147
Viet Tran <vietnamtran@gmail.com>
2020-09-02 17:13:09 +0000

    Ians file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,53 +11,6 @@ from models.state import State
 app = Flask(__name__)
 sto = storage.all('State').values()
 
-
-@app.route('/', strict_slashes=False)
-def hello_hello_hbnb():
-    ''' Hello HBNB! '''
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hello_hbnb():
-    ''' Hello HBNB! '''
-    return "HBNB"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def hello_c(text):
-    ''' Hello HBNB! '''
-    return "C {}".format(text.replace("_", " "))
-
-
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def hello_python(text=None):
-    ''' Hello HBNB! '''
-    if text is not None:
-        return "Python {}".format(text.replace("_", " "))
-    else:
-        return "Python is cool"
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def hello_number(n):
-    ''' Hello HBNB! '''
-    return "{} is a number".format(text)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def hello_number_template(n):
-    ''' Hello HBNB! '''
-    return render_template("5-number.html", n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def hello_number_odd_or_even(n):
-    ''' Hello HBNB! '''
-    return render_template("6-number_odd_or_even.html", n=n)
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     ''' Hello HBNB! '''
