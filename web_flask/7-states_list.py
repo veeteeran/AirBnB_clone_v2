0ab6a85180c3a08137da7ce853f8c717401956fc
Viet Tran <vietnamtran@gmail.com>
2020-09-02 17:06:51 +0000

    Ians file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,73 @@
 #!/usr/bin/python3
-""" Task 8 doc """
+'''
+    Task 8
+'''
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+
+
 app = Flask(__name__)
+sto = storage.all(State).values()
 
 
-@app.teardown_appcontext
-def teardown(self):
-    """ Docstring for teardown """
-    storage.close()
+@app.route('/', strict_slashes=False)
+def hello_hello_hbnb():
+    ''' Hello HBNB! '''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    ''' Hello HBNB! '''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c(text):
+    ''' Hello HBNB! '''
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hello_python(text=None):
+    ''' Hello HBNB! '''
+    if text is not None:
+        return "Python {}".format(text.replace("_", " "))
+    else:
+        return "Python is cool"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def hello_number(n):
+    ''' Hello HBNB! '''
+    return "{} is a number".format(text)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def hello_number_template(n):
+    ''' Hello HBNB! '''
+    return render_template("5-number.html", n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def hello_number_odd_or_even(n):
+    ''' Hello HBNB! '''
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ display a HTML page: (inside the tag BODY) """
-    states = storage.all('State').values()
+    ''' Hello HBNB! '''
+    return render_template("7-states_list.html", sto=sto)
 
-    return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Closes the connection at the end of the request."""
+    storage.close()
 
 if __name__ == '__main__':
-    app.run(
-        host='0.0.0.0',
-        port=5000,
-        debug=True
-    )
+    app.run(host='0.0.0.0', port=5000)
